ac5297fc9d4516a1e1b0b13a2b29f8bfc9f8412e
Connor Chavez <cachavez@wm.edu>
2025-09-09 08:51:44 -0400

    Add files via upload

---
+++ Final Project/Restaurants.py
@@ -0,0 +1,127 @@
+# import necessary packages for the website (imported all of them since I am unsure which ones I will need)
+# no math ones for obvious reasons
+from dash import html, dcc, callback, Input, Output, register_page
+import requests
+import dash_bootstrap_components as dbc
+
+# registers the page and makes it possible to access from the home page
+register_page(__name__, path='/Restaurants', name="Restaurants")
+
+# Williamsburg coordinates
+LAT, LON = 37.2707, -76.7075
+RADIUS = 16000  # ~10 miles in meters
+
+# Cuisine categories mapping
+# multiple for some since they are similar
+# empty list means no filtering on cuisine type
+CUISINE_CATEGORIES = {
+    "American": ["american"],
+    "Asian": ["asian", "chinese", "japanese"], 
+    "BBQ/Barbeque": ["bbq", "barbecue"],
+    "Indian": ["indian"],
+    "Italian/Pizza": ["italian", "pizza"],
+    "Seafood": ["seafood"],
+    "Other": []
+}
+# cuisine image names for chosen cuisine categories
+CUISINE_IMAGES = {
+    "American": "american.jpg",
+    "Asian": "asian.jpg",
+    "BBQ/Barbeque": "bbq.jpg",
+    "Indian": "indian.jpg",
+    "Italian/Pizza": "italian.jpg",
+    "Seafood": "seafood.jpg",
+    "Other": "other.jpg"
+}
+# layout of website
+layout = dbc.Container([ 
+    html.H1("Restaurants in Williamsburg"), # heading
+    html.Label("Select Food Style:"), # title of dropdown
+    dcc.Dropdown(
+        id="cuisine-dd", # cusinies drop down
+        options=[{"label": k, "value": k} for k in CUISINE_CATEGORIES.keys()], # options for cuisine dropdown
+        value="American", # intial setting on dropdown
+        clearable=False # does not allow dropdown to be empty
+    ),
+    html.Br(), 
+    dbc.Button("Search", id="search-btn", n_clicks=0, color="primary"), # button to allow you to search for the restaurant type
+    html.Br(), html.Br(),
+    dbc.Row([
+        dbc.Col([
+            html.Img(id="cuisine-img", style={"width": "100%", "max-width": "400px", "margin-bottom": "20px"}), # image of the type of food
+            dcc.Loading(html.Div(id="restaurant-list"))
+        ], md=7)
+    ])
+], fluid=True)
+
+# function that gets restaurants from Overpass API that matches the cuisine filter
+def fetch_restaurants(cuisine_filter):
+    query = f"""
+    [out:json][timeout:15];
+    (
+      node["amenity"="restaurant"](around:{RADIUS},{LAT},{LON}); 
+      way["amenity"="restaurant"](around:{RADIUS},{LAT},{LON});
+      relation["amenity"="restaurant"](around:{RADIUS},{LAT},{LON});
+    );
+    out center 50;
+    """
+    # gets the restaurant data from the Overpass API
+    # the data query part is the above where the requests only looks for restaurants in that vicinity 
+    try:
+        r = requests.get("https://overpass-api.de/api/interpreter", params={'data': query}, timeout=15) # timeout after 15 seconds
+        r.raise_for_status()
+        data = r.json()["elements"]  # all the restaurants as a list
+    except requests.RequestException:
+        return [] # returns empty list if there is an error
+
+    results = []
+    # loops through the data and filters it based on the cuisine type
+    for restaurant in data:
+        tags = restaurant.get("tags", {})
+        cuisines = tags.get("cuisine", "").lower().split(";") # same format for all cuisines
+        if CUISINE_CATEGORIES[cuisine_filter]: # which restaurants fit in the cuisine filter
+            if not any(c in cuisines for c in CUISINE_CATEGORIES[cuisine_filter]):
+                continue
+        results.append({
+            "name": tags.get("name", "Unnamed"), # name of restaurant
+            "phone": tags.get("phone", "Please refer to the website for a phone number."), # phone number if availible
+            "website": tags.get("website", "There is no website available for this restaurant") # website if availible
+        })
+
+    return results[:9] # top 9 results since there was an error with the 10th one for a certain category (was listed as unnamed and had no information)
+
+
+@callback(
+    Output("restaurant-list", "children"), # children since a text/div
+    Input("search-btn", "n_clicks"), # n_clicks since it is a button
+    Input("cuisine-dd", "value") # value since it is a dropdown
+)
+# callback function that updates the restaurant list when the search button is clicked
+def update_restaurants(n_clicks, cuisine): 
+    restaurants = fetch_restaurants(cuisine) # prints the list of restaurants that match the cuisine type
+    if not restaurants:
+        return html.Div("No restaurants found.") # if there are no restaurants that fit the criteria
+    
+    # prints all the desired information about the restaurant or restaurants
+    children = []  # stores the list in here
+    for r in restaurants:
+        children.append(
+            dbc.Card(
+                dbc.CardBody([
+                    html.H4(r["name"]),
+                    html.P(f"Phone: {r['phone']}"),
+                    html.P(html.A("Website", href=r["website"], target="_blank"))
+                ]),
+                className="mb-3"
+            )
+        )
+    return children
+
+
+@callback(
+    Output("cuisine-img", "src"),
+    Input("cuisine-dd", "value")
+)
+# callback function for the image
+def update_cuisine_image(cuisine):
+    return f"/assets/{CUISINE_IMAGES.get(cuisine, 'other.jpg')}" # shows the image associated with the type of food
